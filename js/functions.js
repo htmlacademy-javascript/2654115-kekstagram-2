7b9e11c8f4857e8e9fe74cae97ebb84223e21583
Nastya <nadasckowa@mail.ru>
2025-05-23 13:48:31 +1200

    Реализованы функции: проверка длины строки, проверка на палиндром

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+function checkStrLenght(str, length){
+
+  return (str.length <= length);
+
+}
+
+function checkStrForPalindrom(str){
+  const strNormalize = str.replaceAll().toUpperCase();
+  const endString = strNormalize.length - 1;
+
+  for(let i = 0; i < Math.floor(strNormalize.length / 2); i++){
+    if(strNormalize[i] !== strNormalize[endString - i]){
+      return false;
+    }
+  }
+
+  return true;
+}
