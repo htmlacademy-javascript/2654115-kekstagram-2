d2ce60062ac658928743426802d3282ed33302eb
Nastya <nadasckowa@mail.ru>
2025-06-20 17:53:36 +1200

    Тест на новом компе

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import {renderFragment} from './util.js';
 import {pictureContainer, createPicture,generatePicture} from './render-picture.js';
-
+/*Тест*/
 renderFragment(generatePicture, createPicture, pictureContainer);
