2ff08a4d896e461ce780a24c18c17c1bf8a7f7cc
Nastya <nadasckowa@mail.ru>
2025-06-28 17:20:01 +1200

    Добавлены функции

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {renderFragment} from './util.js';
 import {pictureContainer, createPicture,generatePicture} from './render-picture.js';
-/*Тест*/
+import './render-big-picture.js'
 renderFragment(generatePicture, createPicture, pictureContainer);
+
+
--- js/render-big-picture.js
+++ js/render-big-picture.js
@@ -1,1 +1,44 @@
-/*Файл отрисовки полного изображения с комментариями */
+/*Действия с окном большого изображения. */
+import { pictureContainer } from "./render-picture";
+const bigPicture = document.querySelector('.big-picture');
+const closeButtonBigPicture = bigPicture.querySelector('.big-picture__cancel');
+
+pictureContainer.addEventListener('click', (evt) => {
+
+  const dataPicture = evt.target.closest('.picture');
+  if(dataPicture){
+    openBigPicture(dataPicture);
+  }
+
+
+})
+
+const onBigPictureKyedownEsc = () =>{
+//функция обработчик нажатия на esc
+
+}
+
+
+const openBigPicture = (picture) =>{
+//удалить класс hidden
+bigPicture.classList.remove('hidden');
+//добавить обработчик нажатия на esc
+//остальная логика
+
+};
+
+const closeBigPicture = () =>{
+//добавить класс hidden
+bigPicture.classList.add('hidden');
+//удалить обработчик нажатия на esc
+
+};
+
+closeButtonBigPicture.addEventListener('click', closeBigPicture);
+
+const renderBigPicture = () =>{
+//подставить значение полученой картинки в класс bidpictures
+
+
+}
+
