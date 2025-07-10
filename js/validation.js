049e0ae4af5ebe14d93a15e0401c859ecf581088
Nastya <nadasckowa@mail.ru>
2025-07-10 20:43:49 +1200

    Исправлена валидация

--- js/validation.js
+++ js/validation.js
@@ -1,7 +1,27 @@
 //Файл валидации хештегов и комментариев
-const MAX_LENGTH_COMMENT = 140;
-const MAX_HASHTAG = 5;
-const MAX_LENGTH_HASHTAG = 20;
+
+const VALIDATION_RULES = {
+  MAX_LENGTH_COMMENT: 140,
+  MAX_HASHTAG: 5,
+  MAX_LENGTH_HASHTAG: 20
+}
+
+const ERROR_MESSAGES = {
+  HASHTAGS: {
+    TOO_MANY: `Нельзя указать больше  ${VALIDATION_RULES.MAX_HASHTAG} хэштегов`,
+    MUST_START_WITH_HASH: 'Хэштег должен начинаться с #',
+    ONLY_HASH: 'Хэштег не может состоять только из одной решётки',
+    TOO_LONG: `Максимальная длина хэштега — ${VALIDATION_RULES.MAX_HASHTAG_LENGTH} символов`,
+    INVALID_CHARACTERS: 'Хэштег может содержать только буквы и цифры, без пробелов и спецсимволов',
+    NOT_UNIQUE: 'Один и тот же хэштег не может использоваться дважды',
+    BY_SPACE: 'Хештеги вводятся через пробел'
+  },
+  COMMENT: {
+    TOO_LONG: `Длина комментария не может превышать ${VALIDATION_RULES.MAX_COMMENT_LENGTH} символов`
+  }
+};
+
+
 
 const uploadForm = document.querySelector('.img-upload__form');
 const commentsgInput = uploadForm.querySelector('.text__description');
@@ -10,7 +30,7 @@ const hashtagInput = uploadForm.querySelector('.text__hashtags');
 
 const isValidComment = () =>{
 
-    if(commentsgInput.value.length > MAX_LENGTH_COMMENT){
+    if(commentsgInput.value.length > VALIDATION_RULES.MAX_LENGTH_COMMENT){
       return false;
     }
     return true;
@@ -25,34 +45,39 @@ const isValidHastag = () =>{
     if(hashtagInput.value != 0){
       const hashtags = hashtagInput.value.toLowerCase().trim().split(/\s+/);
       const uniqueHashtags = new Set();
+      const regularExHashtag = /^#([а-яёa-z0-9]){1,19}$/i;
       messageErrorHastag = '';
 
 
-      if (hashtags.length > MAX_HASHTAG) {
-      messageErrorHastag =`Не может быть больше ${MAX_HASHTAG} хэштегов.`;
+      if (hashtags.length > VALIDATION_RULES.MAX_HASHTAG) {
+      messageErrorHastag = ERROR_MESSAGES.HASHTAGS.TOO_MANY;
       return false;
       }
 
       for (const tag of hashtags) {
         // Проверка на корректный формат
         if(tag.length === 1 && tag[0]==='#'){
-          messageErrorHastag ='Хештег не может состоять из одного символа #';
+          messageErrorHastag = ERROR_MESSAGES.HASHTAGS.ONLY_HASH;
           return false;
         }
         if(tag[0] != '#'){
-          messageErrorHastag ='Хештег должен начинаться с символа #';
+          messageErrorHastag = ERROR_MESSAGES.HASHTAGS.MUST_START_WITH_HASH;
           return false;
         }
-        if(tag.length > MAX_LENGTH_HASHTAG){
-          messageErrorHastag ='Слишком длинный хештег';
+        if(tag.length > VALIDATION_RULES.MAX_LENGTH_HASHTAG){
+          messageErrorHastag = ERROR_MESSAGES.HASHTAGS.TOO_LONG;
           return false;
         }
-        if (!/^#([а-яёa-z0-9]){1,19}$/i.test(tag)) {
-          messageErrorHastag ='Хештег содержит запрещенные символы';
+        if(tag.split('#').length >2){
+          messageErrorHastag = ERROR_MESSAGES.HASHTAGS.BY_SPACE;
+          return false;
+        }
+        if (!regularExHashtag.test(tag)) {
+          messageErrorHastag = ERROR_MESSAGES.HASHTAGS.INVALID_CHARACTERS;
           return false;
         }
         if (uniqueHashtags.has(tag)) {
-          messageErrorHastag =`Хештег ${tag} дублируется`;
+          messageErrorHastag = ERROR_MESSAGES.HASHTAGS.NOT_UNIQUE;
           return false;
         }
 
