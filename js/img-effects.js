9130817f127485583beb642d529f306be8273230
Nastya <nadasckowa@mail.ru>
2025-07-11 22:54:33 +1200

    Добавлены параметры для слайдера

--- js/img-effects.js
+++ js/img-effects.js
@@ -1,4 +1,71 @@
 const SCALE_STEP = 0.25;
+
+
+const slliderOptionNone = {
+  range:{
+   min: 0,
+   max: 100
+  },
+  start: 100,
+  step: 1
+};
+
+const slliderOptionHrome = {
+  range:{
+   min: 0,
+   max: 1
+  },
+  start: 1,
+  step: 0.1
+};
+
+const slliderOptionSepia = {
+  range:{
+   min: 0,
+   max: 1
+  },
+  start: 1,
+  step: 0.1
+};
+
+const slliderOptionInvert = {
+  range:{
+   min: 0,
+   max: 100
+  },
+  start: 100,
+  step: 1
+};
+
+const slliderOptionBlur = {
+  range:{
+   min: 0,
+   max: 3
+  },
+  start: 3,
+  step: 0.1
+};
+
+const slliderOptionBrightness = {
+  range:{
+   min: 0,
+   max: 3
+  },
+  start: 3,
+  step: 0.1
+};
+
+const effects = {
+
+  none: slliderOptionNone,
+  hrome: slliderOptionHrome,
+  sepia: slliderOptionSepia,
+  inver: slliderOptionInvert,
+  blur: slliderOptionBlur,
+  brightness: slliderOptionBrightness
+
+};
+
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 //const uploadEffectLevel = document.querySelector('.img-upload__effect-level');
 
@@ -9,7 +76,10 @@ const imageScaleValue = uploadOverlay.querySelector('.scale__control--value');
 const smaller = uploadScale.querySelector('.scale__control--smaller');//уменьшение
 const bigger = uploadScale.querySelector('.scale__control--bigger');//увеличение
 
+const slider = uploadOverlay.querySelector('.effect-level__slider');
+
 let scale = 1;
+
 const onButtonSmallerClick = () =>{
     if(scale > SCALE_STEP){
       scale -= SCALE_STEP;
@@ -23,8 +93,9 @@ const onButtonBiggerClick = () =>{
     if(scale < 1){
       scale += SCALE_STEP;
       image.style.transform = `scale(${scale})`;
-      imageScaleValue.value = `${scale * 100}%`
+      imageScaleValue.value = `${scale * 100}%`;
     }
 }
+
 
 export {uploadOverlay, smaller,bigger, onButtonSmallerClick, onButtonBiggerClick};
