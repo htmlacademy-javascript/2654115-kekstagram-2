03ae7c58e453e578fd8e06dccd0e23d6705aa155
Nastya <nadasckowa@mail.ru>
2025-07-19 08:02:35 +1200

    Адрес перенесен в константу

--- js/api.js
+++ js/api.js
@@ -1,5 +1,12 @@
+const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+
 const getData = () => fetch(
-  'https://31.javascript.htmlacademy.pro/kekstagram/data')
+  `${BASE_URL}${Route.GET_DATA}`)
   .then((response) => response.json());
 
   export { getData }
