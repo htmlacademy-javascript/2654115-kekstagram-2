7a5b183801d5910f23bc7100ae9fef12e37a70ab
Nastya <nadasckowa@mail.ru>
2025-07-20 20:58:02 +1200

    Открыта инициализация фильтров и фильтрация

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ import { pictureContainer, createPicture } from './render-picture.js';
 import { renderBigPictures } from './render-big-picture.js';
 import { initUpload } from './upload-form.js';
 import { renderMessageErrorGetData } from './message.js';
-//import { initFilters, renderFiltersImages } from './filters.js';
+import { initFilters, renderFiltersImages } from './filters.js';
 getData()
   .then((image) => {
     renderFragment(image, createPicture, pictureContainer);
-    //initFilters();
-    //renderFiltersImages(image);
+    initFilters();
+    renderFiltersImages(image);
     renderBigPictures(image);
   }).catch(renderMessageErrorGetData);
 
